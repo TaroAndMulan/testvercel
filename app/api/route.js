420656bd0925231fd51a73d5d6b3a1dfbc02f8a8
c.yosnai <6372096621@student.chula.ac.th>
2024-01-07 05:59:39 +0700

    wtf

---
+++ app/api/route.js
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import { exec } from "child_process";
+
+export async function POST(request) {
+	const { text } = await request.json();
+
+	const translatedTextPromise = new Promise((resolve, reject) => {
+
+		exec(
+			`pypy\\Scripts\\activate.bat && python component\\pytest.py`,
+			(error, stdout, stderr) => {
+				if (error) {
+					console.error(`exec error: ${error}`);
+					reject(error);
+				}
+				resolve(stdout);
+			}
+		);
+	});
+
+	const translatedText = await translatedTextPromise;
+	return NextResponse.json({ translatedText });
+}
---
+++ component/table.js
@@ -0,0 +1,36 @@
+function generateBody(){
+
+}
+
+export default function Table(){
+    return (
+        <>
+        <table className="border border-red-500">
+  <thead>
+    <tr>
+      <th>วัน</th>
+      <th>เวร ER</th>
+      <th>เวร Ward</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>ยศนัย</td>
+      <td>ยศนาม</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>ยศนาม</td>
+      <td>ยศนัย</td>
+    </tr>
+    <tr>
+      <td>Shining Star</td>
+      <td>Earth, Wind, and Fire</td>
+      <td>1975</td>
+    </tr>
+  </tbody>
+</table>
+        </>
+    )
+}
