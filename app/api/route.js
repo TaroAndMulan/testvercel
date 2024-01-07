6cc6b8404b97374172ce0096eec31257b72cc809
c.yosnai <6372096621@student.chula.ac.th>
2024-01-07 07:01:21 +0700

    wtf

--- app/api/route.js
+++ app/api/route.js
@@ -13,7 +13,6 @@ export async function POST(request) {
 					console.error(`exec error: ${error}`);
 					reject(error);
 				}
-				console.log(stdout)
 				resolve(stdout);
 			}
 		);
--- app/page.js
+++ app/page.js
@@ -21,6 +21,8 @@ export default function Home() {
 			body: JSON.stringify("xxx" ),
 		});
 		const { translatedText } = await res.json();
+    console.log(translatedText)
+
 		setSolution(translatedText);
 	};
 
